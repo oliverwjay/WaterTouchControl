cd105dc9298934ee54304266f84eed40d507250b
oliverwjay <oliverbuildsbots@gmail.com>
2017-11-05 21:02:25 -0500

    Added wool feed second motor

--- WoolFeed.cpp
+++ WoolFeed.cpp
@@ -3,12 +3,17 @@
 #include "Menu.h"
 #include "Status.h"
 
-#define DRIVE_CHANNEL 12
-#define DRIVE_BRAKE 9
-#define DRIVE_PWM 3
+#define DRIVE_CHANNEL_A 12
+#define DRIVE_BRAKE_A 9
+#define DRIVE_PWM_A 3
+#define DRIVE_CHANNEL_B 13
+#define DRIVE_BRAKE_B 8
+#define DRIVE_PWM_B 11
 #define SERVO_PORT 5
 #define CIRCULATION_PUMP_RELAY 42
 #define WOOL_SENSE_PIN A15
+
+#define STRETCH_TIME 500 //in ms
 
 //Servo servo;
 
@@ -18,8 +23,10 @@ bool statusWFEnabled = false;
 bool statusWasWFEnabled = false;
 
 void initWoolFeed() {
-  pinMode(DRIVE_CHANNEL, OUTPUT); // set up motor drive
-  pinMode(DRIVE_BRAKE, OUTPUT);
+  pinMode(DRIVE_CHANNEL_A, OUTPUT); // set up motor drive
+  pinMode(DRIVE_BRAKE_A, OUTPUT);
+  pinMode(DRIVE_CHANNEL_B, OUTPUT); // set up motor drive
+  pinMode(DRIVE_BRAKE_B, OUTPUT);
   pinMode(CIRCULATION_PUMP_RELAY, OUTPUT);
 
   //  servo.attach(SERVO_PORT);
@@ -73,7 +80,10 @@ void updateWoolFeed() {
       }
       break;
     case WFS_EXTENDING:
-      if (millis() - lastWFSwitch > getVar(MVI_WF_REV_TIME) * 1000L) setWoolFeedState(WFS_IDLE);
+      if (millis() - lastWFSwitch > getVar(MVI_WF_REV_TIME) * 1000L) setWoolFeedState(WFS_STRETCHING);
+      break;
+    case WFS_STRETCHING:
+      if (millis() - lastWFSwitch > STRETCH_TIME) setWoolFeedState(WFS_IDLE);
       break;
   }
 }
@@ -86,18 +96,33 @@ void setWoolFeedState(WF_STATE newState) {
     setVar(MVI_WF_SWITCH, newState > WFS_OFF);
   }
   if (newState == WFS_MAN_REVERSING || newState == WFS_REVERSING) {
-    digitalWrite(DRIVE_CHANNEL, LOW);
-    digitalWrite(DRIVE_BRAKE, LOW);
-    analogWrite(DRIVE_PWM, getVar(MVI_WF_PWR));
+    digitalWrite(DRIVE_CHANNEL_A, LOW);
+    digitalWrite(DRIVE_BRAKE_A, LOW);
+    analogWrite(DRIVE_PWM_A, getVar(MVI_WF_PWR));
+    digitalWrite(DRIVE_CHANNEL_B, LOW);
+    digitalWrite(DRIVE_BRAKE_B, LOW);
+    analogWrite(DRIVE_PWM_B, getVar(MVI_WF_PWR));
   }
   else if (newState == WFS_MAN_FEEDING || newState == WFS_FEEDING || newState == WFS_EXTENDING) {
-    digitalWrite(DRIVE_CHANNEL, HIGH);
-    digitalWrite(DRIVE_BRAKE, LOW);
-    analogWrite(DRIVE_PWM, getVar(MVI_WF_PWR));
+    digitalWrite(DRIVE_CHANNEL_A, HIGH);
+    digitalWrite(DRIVE_BRAKE_A, LOW);
+    analogWrite(DRIVE_PWM_A, getVar(MVI_WF_PWR));
+    digitalWrite(DRIVE_CHANNEL_B, HIGH);
+    digitalWrite(DRIVE_BRAKE_B, LOW);
+    analogWrite(DRIVE_PWM_B, getVar(MVI_WF_PWR));
+  }
+  else if (newState == WFS_STRETCHING) {
+    digitalWrite(DRIVE_BRAKE_A, HIGH);
+    analogWrite(DRIVE_PWM_A, 0);
+    digitalWrite(DRIVE_CHANNEL_B, HIGH);
+    digitalWrite(DRIVE_BRAKE_B, LOW);
+    analogWrite(DRIVE_PWM_B, getVar(MVI_WF_PWR));
   }
   else {
-    digitalWrite(DRIVE_BRAKE, HIGH);
-    analogWrite(DRIVE_PWM, 0);
+    digitalWrite(DRIVE_BRAKE_A, HIGH);
+    analogWrite(DRIVE_PWM_A, 0);
+    digitalWrite(DRIVE_BRAKE_B, HIGH);
+    analogWrite(DRIVE_PWM_B, 0);
   }
   if (newState == WFS_OFF) {
     digitalWrite(CIRCULATION_PUMP_RELAY, LOW);
